38e12fc8201d4dfc0cb818a766b6e7eb35b3a46e
Rafael Nossal <rafael.nossal@gmail.com>
2021-06-14 19:31:40 -0300

    Correção de nomes de arquivo

--- api/models/index.js
+++ api/models/index.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
-import Course from './Course.js';
+import Course from './course.js';
 import FollowUp from './followUp.js';
 import Jwt from './jwt.js';
-import Lead from './Lead.js';
+import Lead from './lead.js';
 import User from './user.js';
 
 const connect = () => mongoose.connect(process.env.DB_ADDRESS, {
